db8eb6d1e56c342fe5b58099afc6985c7cbc69fb
Hernan Case <her.case@live.com.ar>
2020-07-31 14:29:36 -0300

    ADDED Custom Slider with State

--- src/components/ChannelsGrid/index.js
+++ src/components/ChannelsGrid/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './style.sass';
 
 import api from 'api';
@@ -8,36 +8,52 @@ import { playVideo } from 'redux/actions/player-actions'
 import { useDispatch, useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faArrowRight, faPlus, faPlayCircle, faPlay } from '@fortawesome/free-solid-svg-icons'
 import { useHistory } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 
 const GridComponent = () => {
-  let [response, setResponse] = useState([]);
   let [ isLoading, setIsLoading ] = useState(true);
+  let [ grid , setGrid] = useState([]) 
   let channel = useSelector( state => state.currentChannel)
+
+  const [, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
 
   useEffect(()=>{
     api.getChannels()
     .then((response)=>{
-        setResponse(response.data)
+        setGrid(response.data)
         setIsLoading(false)
     })
     .catch((error) => {
         console.log(error)
     })
+  }, [] )
 
-  }, [channel] )
-
-  function generateChannels(){
-    let list = response.map( (channel, index) =>  
-      <Channel  key={index} 
-                id={channel.channel_id}
-                title={channel.title}
-                url={channel.media.url}
-                isLoading={isLoading}
+  function generateChannels(){  
+    let list = grid.map( (channel, index) =>  
+      <ChannelCover  
+        key={index} 
+        index={index}
+        id={channel.channel_id}
+        title={channel.title}
+        url={channel.media.url}
+        isLoading={isLoading}
     />);
     return list;
+  }
+
+  const prevCover = () => {
+    let last = grid.pop()
+    grid.unshift(last)
+    forceUpdate(last);
+  }
+
+  const nextCover = () => {
+    let first = grid.shift()
+    grid.push(first)
+    forceUpdate(first);
   }
 
   return(
@@ -46,41 +62,55 @@ const GridComponent = () => {
         <div className="grid-title">
           <p>Lista de canales</p>
           <div className="grid-controls">
-            <FontAwesomeIcon icon={faArrowLeft} size='xs' />
-            <FontAwesomeIcon icon={faArrowRight} size='xs' />
+            <button onClick={() => prevCover()}>
+              <FontAwesomeIcon icon={faArrowLeft} size='lg'/>
+            </button>
+            <button onClick={() => nextCover()}>
+              <FontAwesomeIcon icon={faArrowRight} size='lg'/>
+            </button>
           </div>
         </div>
-        <div className="grid-channels">
-          {response && generateChannels()}
+        <div className="grid-content">
+          {grid && generateChannels()}
         </div>
       </div>
     </section>
   );
 };
 
-const Channel = ({id, title, url, isLoading}) => {
+const ChannelCover = ({index, id, title, url, isLoading}) => {
   const history = useHistory();
   const dispatch = useDispatch()
+  const [isHome, setisHome] = useState()
+
+  useEffect(()=>{
+    if (window.location.hash === "#/"){
+      setisHome(true)
+    } else {
+      setisHome(false)
+    }
+  },[window.location.hash])
 
   const redirectandPlay = () => {
     history.push(`/channel/${id}`)
     dispatch(playVideo(title, url))
   }
 
+  let visible = (index < 5) ? " active" : "";
+  let size = isHome ? " large" : "";
+
   return (
-      <div className={ isHome() ? "channel" : "channel small" } onClick={ () => redirectandPlay()} >
+      <div className={ "cover" + visible + size} onClick={ () => redirectandPlay()} >
         { isLoading &&<Skeleton className="skeleton" /> }
         <img 
           alt={id} 
           src={require(`../../assets/images/covers/${id}.jpg`)}
         />
+        <div className="play_button">
+          <FontAwesomeIcon className="" icon={faPlay} size="2x" color="#fff"/>
+        </div>
       </div>
   );
-}
-
-const isHome = () => {
-  let response = ( window.location.pathname === "/" ) ? true : false;
-  return response;
 }
 
 export default GridComponent;
