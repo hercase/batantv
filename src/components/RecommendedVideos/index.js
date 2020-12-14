2a3207f554ffafe22f058cafe36a5d86430560be
Cristian Viterales <cviterales@batan.coop>
2020-12-14 10:43:16 -0300

    fixed problems and added new channel on grid

--- src/components/RecommendedVideos/index.js
+++ src/components/RecommendedVideos/index.js
@@ -10,17 +10,17 @@ const RecommendedComponent = () => {
         <div className="recommended-grid">
           <div className="recommended-item">
             <div className="recommended-wrap">
-              <iframe title="Video 1" className="recommended-item" src="https://www.youtube.com/embed/hhOzPrEpoSg?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe title="Video 1" className="recommended-item" src="https://www.youtube.com/embed/hhOzPrEpoSg?controls=0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
           </div>
           <div className="recommended-item">
             <div className="recommended-wrap">
-              <iframe title="Video 2" className="recommended-item" src="https://www.youtube.com/embed/5CK6IyxSGPo?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe title="Video 2" className="recommended-item" src="https://www.youtube.com/embed/5CK6IyxSGPo?controls=0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
           </div>
           <div className="recommended-item">
             <div className="recommended-wrap">
-              <iframe title="Video 3" className="recommended-item" src="https://www.youtube.com/embed/J1ZBSAM4MjA?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe title="Video 3" className="recommended-item" src="https://www.youtube.com/embed/J1ZBSAM4MjA?controls=0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
           </div>
         </div>
